804715c963a614268ebcf405455509a7b6fd298e
jhonatanborg <jhonatanborgesdj@gmail.com>
2020-04-18 03:37:31 -0400

    redesign

--- src/components/TopHome.vue
+++ src/components/TopHome.vue
@@ -9,21 +9,15 @@
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
-      <v-btn text color="white">Iniciar sessão</v-btn>
+      <v-btn dark color="white">Iniciar sessão</v-btn>
     </v-toolbar>
-    <v-card
-      height="150"
-      flat
-      color="blue"
-      class="card-home pt-5 overflow-hidden"
-    >
+    <v-card height="150" color="blue" class="card-home pt-5 overflow-hidden">
       <div>
         <div class=" col-sm-12 mx-auto d-flex justify-center">
           <div class="col-sm-6">
             <v-text-field
               prepend-inner-icon="mdi-magnify"
               solo
-              flat
               label="Pesquise por cidade ou estado
 "
             ></v-text-field>
@@ -35,8 +29,8 @@
       </div>
     </v-card>
     <div class="col-sm-12 mx-auto">
-      <v-row class="mx-auto">
-        <v-card flat class="col-sm-2">
+      <!-- <v-row class="mx-auto"> -->
+      <!-- <v-card class="col-sm-2 elevation-0">
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title class="title-category">
@@ -60,90 +54,207 @@
               <v-list-item-title>Freelance</v-list-item-title>
             </v-list-item>
           </v-list>
-        </v-card>
+        </v-card> -->
 
-        <v-card flat class="col-sm-10">
-          <v-list-item
-            color="blue"
-            link
-            class="line-job mx-5 d-flex justify-start align-center"
-          >
-            <div class="d-flex col-sm-6">
-              <div class="mr-3">
-                <v-img
-                  width="50px"
-                  src="https://img.utdstc.com/icons/facebook-android.png:75"
-                ></v-img>
-              </div>
-              <div class="mr-3">
-                <span class="job-title">
-                  <b> Auxiliar cozinha comunitária</b>
-                </span>
-                <br />
-                <small>Facebook CO. - postado a 4 dias</small>
-              </div>
+      <v-card class="col-sm-10 elevation-0">
+        <v-list-item
+          color="blue"
+          link
+          class="line-job mx-5 d-flex justify-start align-center"
+        >
+          <div class="d-flex col-sm-6">
+            <div class="mr-3">
+              <v-img
+                width="50px"
+                src="https://img.utdstc.com/icons/facebook-android.png:75"
+              ></v-img>
             </div>
-
-            <div class="col-sm-3">
-              <span class="category-title">
-                <b> Freelance</b>
+            <div class="mr-3">
+              <span class="job-title">
+                <b> Auxiliar cozinha comunitária</b>
               </span>
               <br />
-              <small>Sinop, MT</small>
+              <small>Facebook CO. - postado a 4 dias</small>
             </div>
-            <div class="col-sm-2">
-              <v-chip outlined pill color="red" dark>Cozinha</v-chip>
-            </div>
-            <div>
-              <v-btn flat icon color="primary">
-                <v-icon> mdi-bookmark-outline</v-icon>
-              </v-btn>
-            </div>
-          </v-list-item>
-          <div class="col-sm-12">
-            <v-divider class="mx-5"></v-divider>
           </div>
-          <v-list-item
-            link
-            class="line-job mx-5 d-flex justify-start align-center"
-          >
-            <div class="d-flex col-sm-6">
-              <div class="mr-3">
-                <v-img
-                  width="50px"
-                  src="https://img.utdstc.com/icons/facebook-android.png:75"
-                ></v-img>
-              </div>
-              <div class="mr-3">
-                <span class="job-title">
-                  <b> Auxiliar cozinha comunitária</b>
-                </span>
-                <br />
-                <small>Facebook CO. - postado a 4 dias</small>
-              </div>
-            </div>
 
-            <div class="col-sm-3">
-              <span class="category-title">
-                <b> Freelance</b>
+          <div class="col-sm-3">
+            <span class="category-title">
+              <b> Freelance</b>
+            </span>
+            <br />
+            <small>Sinop, MT</small>
+          </div>
+          <div class="col-sm-2">
+            <v-chip outlined pill color="red" dark>Cozinha</v-chip>
+          </div>
+          <div>
+            <v-btn icon color="primary">
+              <v-icon> mdi-bookmark-outline</v-icon>
+            </v-btn>
+          </div>
+        </v-list-item>
+        <div class="col-sm-12">
+          <v-divider class="mx-5"></v-divider>
+        </div>
+        <v-list-item
+          link
+          class="line-job mx-5 d-flex justify-start align-center"
+        >
+          <div class="d-flex col-sm-6">
+            <div class="mr-3">
+              <v-img
+                width="50px"
+                src="http://casadocarvalho.net/wp-content/uploads/2018/06/canto-da-terra.png"
+              ></v-img>
+            </div>
+            <div class="mr-3">
+              <span class="job-title">
+                <b> Auxiliar cozinha comunitária</b>
               </span>
               <br />
-              <small>Sinop, MT</small>
+              <small>Facebook CO. - postado a 4 dias</small>
             </div>
-            <div class="col-sm-2">
-              <v-chip outlined pill color="red" dark>Cozinha</v-chip>
-            </div>
-            <div>
-              <v-btn flat icon color="primary">
-                <v-icon> mdi-bookmark-outline</v-icon>
-              </v-btn>
-            </div>
-          </v-list-item>
-          <div class="col-sm-12">
-            <v-divider class="mx-5"></v-divider>
           </div>
-        </v-card>
-      </v-row>
+
+          <div class="col-sm-3">
+            <span class="category-title">
+              <b> Freelance</b>
+            </span>
+            <br />
+            <small>Sinop, MT</small>
+          </div>
+          <div class="col-sm-2">
+            <v-chip outlined pill color="red" dark>Cozinha</v-chip>
+          </div>
+          <div>
+            <v-btn icon color="primary">
+              <v-icon> mdi-bookmark-outline</v-icon>
+            </v-btn>
+          </div>
+        </v-list-item>
+        <div class="col-sm-12">
+          <v-divider class="mx-5"></v-divider>
+        </div>
+        <v-list-item
+          link
+          class="line-job mx-5 d-flex justify-start align-center"
+        >
+          <div class="d-flex col-sm-6">
+            <div class="mr-3">
+              <v-img
+                width="50px"
+                src="http://casadocarvalho.net/wp-content/uploads/2018/06/canto-da-terra.png"
+              ></v-img>
+            </div>
+            <div class="mr-3">
+              <span class="job-title">
+                <b> Auxiliar cozinha comunitária</b>
+              </span>
+              <br />
+              <small>Facebook CO. - postado a 4 dias</small>
+            </div>
+          </div>
+
+          <div class="col-sm-3">
+            <span class="category-title">
+              <b> Freelance</b>
+            </span>
+            <br />
+            <small>Sinop, MT</small>
+          </div>
+          <div class="col-sm-2">
+            <v-chip outlined pill color="red" dark>Cozinha</v-chip>
+          </div>
+          <div>
+            <v-btn icon color="primary">
+              <v-icon> mdi-bookmark-outline</v-icon>
+            </v-btn>
+          </div>
+        </v-list-item>
+        <div class="col-sm-12">
+          <v-divider class="mx-5"></v-divider>
+        </div>
+        <v-list-item
+          link
+          class="line-job mx-5 d-flex justify-start align-center"
+        >
+          <div class="d-flex col-sm-6">
+            <div class="mr-3">
+              <v-img
+                width="50px"
+                src="http://casadocarvalho.net/wp-content/uploads/2018/06/canto-da-terra.png"
+              ></v-img>
+            </div>
+            <div class="mr-3">
+              <span class="job-title">
+                <b> Auxiliar cozinha comunitária</b>
+              </span>
+              <br />
+              <small>Facebook CO. - postado a 4 dias</small>
+            </div>
+          </div>
+
+          <div class="col-sm-3">
+            <span class="category-title">
+              <b> Freelance</b>
+            </span>
+            <br />
+            <small>Sinop, MT</small>
+          </div>
+          <div class="col-sm-2">
+            <v-chip outlined pill color="red" dark>Cozinha</v-chip>
+          </div>
+          <div>
+            <v-btn icon color="primary">
+              <v-icon> mdi-bookmark-outline</v-icon>
+            </v-btn>
+          </div>
+        </v-list-item>
+        <div class="col-sm-12">
+          <v-divider class="mx-5"></v-divider>
+        </div>
+        <v-list-item
+          link
+          class="line-job mx-5 d-flex justify-start align-center"
+        >
+          <div class="d-flex col-sm-6">
+            <div class="mr-3">
+              <v-img
+                width="50px"
+                src="http://casadocarvalho.net/wp-content/uploads/2018/06/canto-da-terra.png"
+              ></v-img>
+            </div>
+            <div class="mr-3">
+              <span class="job-title">
+                <b> Auxiliar cozinha comunitária</b>
+              </span>
+              <br />
+              <small>Facebook CO. - postado a 4 dias</small>
+            </div>
+          </div>
+
+          <div class="col-sm-3">
+            <span class="category-title">
+              <b> Freelance</b>
+            </span>
+            <br />
+            <small>Sinop, MT</small>
+          </div>
+          <div class="col-sm-2">
+            <v-chip outlined pill color="red" dark>Cozinha</v-chip>
+          </div>
+          <div>
+            <v-btn icon color="primary">
+              <v-icon> mdi-bookmark-outline</v-icon>
+            </v-btn>
+          </div>
+        </v-list-item>
+        <div class="col-sm-12">
+          <v-divider class="mx-5"></v-divider>
+        </div>
+      </v-card>
+      <!-- </v-row> -->
     </div>
   </div>
 </template>
